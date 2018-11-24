168f8f995bd5bb4e003ecb60f5e61adc78ba9bf1
root <root@aladinlabs.com>
2018-11-24 20:45:13 +0000

    a

--- public_org/pullFront.php
+++ public_org/pullFront.php
@@ -3,7 +3,7 @@
 	echo exec("cd ..");
 	echo exec("cd EMS-FrontEnd");
 	echo exec("git pull --force");
-	echo exec("ng build");
+	echo exec("ng build --delete-output-path false");
 	echo "build";
 	echo exec("cd ..");
 	echo exec("cd ems.aladinlabs.com");
