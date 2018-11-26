9ff201290adcc2969c21153a5f9a79c03ecf478e
Your Name <bhanuka2017@gmail.com>
2018-11-26 18:41:25 +0530

    disable frontend

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 Route::get('auth/login', function () {
     return json_encode(['error' => ['message' => "Invalid token"]]);
