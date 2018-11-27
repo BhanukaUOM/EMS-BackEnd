de8fb4690357bd5ad2750a9695e807755a96cf91
Your Name <bhanuka2017@gmail.com>
2018-11-27 22:58:28 +0530

    a

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -68,7 +68,7 @@ class AuthController extends Controller
                 'message' => 'Unauthorized'
             ], 401);
         $user = $request->user();
-        $res = User::with('roles')->find($user->id)->get();
+        $res = User::find($user->id)->with('roles');
         $tokenResult = $user->createToken('Personal Access Token');
         $token = $tokenResult->token;
         if ($request->remember_me)
