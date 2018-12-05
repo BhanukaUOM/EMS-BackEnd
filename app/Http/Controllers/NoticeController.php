8da0247e391a8705aa6ade64088f9b3edec5845d
Your Name <bhanuka2017@gmail.com>
2018-12-05 14:30:09 +0530

    a

--- app/Http/Controllers/NoticeController.php
+++ app/Http/Controllers/NoticeController.php
@@ -22,13 +22,17 @@ class NoticeController extends Controller
         $user_roles = Auth::user()->roles;
         $res = [];
         for($i=0; $i<count($user_roles); $i++){
-            $res.append([2,3]);
+            array_push($res, DB::select('
+            SELECT n.id, n.content, n.created_at, n.updated_at
+            FROM user_has__notices ns, notices n
+            WHERE ns.role = ?
+        ', [$user_roles[$i]])->get());
         };
-        // $res.append(DB::select('
-        //     SELECT n.id, n.content, n.created_at, n.updated_at
-        //     FROM user_has__notices ns, notices n
-        //     WHERE ns.user_id = ?
-        // ', [$user_id])->get());
+        array_push($res, DB::select('
+            SELECT n.id, n.content, n.created_at, n.updated_at
+            FROM user_has__notices ns, notices n
+            WHERE ns.user_id = ?
+        ', [$user_id])->get());
         //return $user_roles;
         return json_encode($res);
         //return response()->json(User::find($user)->notices(), 200);
