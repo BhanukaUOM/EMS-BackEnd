4f25ea1e8df7794b2ede35c5ca5788aeaaf80b8c
Your Name <bhanuka2017@gmail.com>
2019-01-20 12:12:02 +0530

    a

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,8 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('address');
-            $table->string('about');
+            $table->string('address')->nullable();
+            $table->string('about')->nullable();
             $table->unsignedInteger('school_id');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
