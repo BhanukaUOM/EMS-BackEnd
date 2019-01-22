e05ae37da5f2d5ee277c7e37c9256565a5850239
Your Name <bhanuka2017@gmail.com>
2019-01-23 00:04:19 +0530

    a

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -245,9 +245,19 @@ class AttendanceController extends Controller
         $res = [];
         for($i=0; $i<count($attendance); $i++){
             if($attendance[$i]->state==true){
-                array_push($res, json_decode('{"'.$attendance[$i]->year . '-' . $attendance[$i]->month . '-' . $attendance[$i]->day . '" : { "customStyles": { "container": { "backgroundColor": "green"}, "text": { "color": "black", "fontWeight": "bold" } } } }'));
+                array_push($res,
+                [
+                    'date'=>$attendance[$i]->year . '-' . $attendance[$i]->month . '-' . $attendance[$i]->day,
+                    'status'=>'green'
+                ]
+            );
             } else {
-                array_push($res, json_decode('{"'.$attendance[$i]->year . '-' . $attendance[$i]->month . '-' . $attendance[$i]->day . '" : { "customStyles": { "container": { "backgroundColor": "red"}, "text": { "color": "black", "fontWeight": "bold" } } } }'));
+                array_push($res,
+                [
+                    'date'=>$attendance[$i]->year . '-' . $attendance[$i]->month . '-' . $attendance[$i]->day,
+                    'status'=>'red'
+                ]
+                //array_push($res, json_decode('{"'.$attendance[$i]->year . '-' . $attendance[$i]->month . '-' . $attendance[$i]->day . '" : { "customStyles": { "container": { "backgroundColor": "red"}, "text": { "color": "black", "fontWeight": "bold" } } } }'));
             }
         }
 
