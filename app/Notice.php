1bfabf89758d427949e1efbf1b08095b72800fca
Your Name <bhanuka2017@gmail.com>
2019-01-23 16:50:48 +0530

    a

--- app/Notice.php
+++ app/Notice.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Notice extends Model
 {
+    protected $fillable = [
+        'user_id', 'notice_id',
+    ];
+
     public function users()
     {
         return $this->belongsToMany(User::class);
